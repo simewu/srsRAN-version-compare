9745a0c1a158c1da769ba9158e844caa99fcc3ba
Simeon Wuthier <simeonwuthier@gmail.com>
2022-10-12 18:18:55 -0600

    Update figures

--- merkle_tree_construction/version_comparison.m
+++ merkle_tree_construction/version_comparison.m
@@ -2,16 +2,48 @@ data = readmatrix('logDirectoryOutput.csv');
 data2 = readmatrix('Algorithm_benchmark_CI_output.csv')
 data_str = readtable('logDirectoryOutput.csv');
 
+% plotNum = 0 = Percent code files / bytes changed (CDF)
 % plotNum = 1 = Percent code files / bytes changed
-% plotNum = 2 = Tree construction
-% plotNum = 3 = Gen/Verify
-plotNum = 1
+% plotNum = 2 = Proof Generation
+% plotNum = 3 = Proof Verification
+plotNum = 2
 
 
 fontSize = 28;
 f = figure;
 
-if plotNum == 1
+
+xoffset = 0.3
+
+if plotNum == 0
+    %x = strcat(table2array(data_str(1:end - 1, 1)) , " – " , table2array(data_str(2:end, 1)))
+    y = data(2:end, 16);
+    y_B = data(2:end, 18);
+    
+    hold on;
+    cdfplot(y)
+    cdfplot(y_B)
+    %xticks(1:length(x))
+    %xticklabels(x)
+    
+    xlabel('Code files changed')
+    ylabel('Difference (%)')
+    xtickangle(60);
+    
+    %axis square;
+    ylim([0, 1])
+    set(gca, 'YGrid', 'on', 'YMinorGrid', 'on');
+    set(gca, 'XScale', 'log');
+    %set(gca, 'XScale', 'log', 'XTick',x_avg, 'XTickLabel', x_avg, 'YGrid', 'on', 'YMinorGrid', 'on');
+    %set(gca, 'XTick',x_avg, 'XTickLabel', x_avg_real, 'YGrid', 'on', 'YMinorGrid', 'on');
+    set(gca,'FontSize', fontSize);
+    
+    legend('Bytes of code', 'Number of files', 'Location', 'NorthWest', 'NumColumns', 2)
+    % ax = gca
+    % ax.XAxis.FontSize = fontSize;
+    % ax.YAxis.FontSize = fontSize;
+
+elseif plotNum == 1
     % "rows = 2:end" is because this is a difference plot,
     % so the first version is "N/A"
     x = strcat(table2array(data_str(1:end - 1, 1)) , " – " , table2array(data_str(2:end, 1)))
@@ -19,14 +51,16 @@ if plotNum == 1
     y_B = data(2:end, 19);
     
     hold on;
+    %bar([y, y_B], 'FaceColor', '#444');
+    xaltoffset = 0.2
     for i=1:length(y)
-        if y_B(i) < y(i)
-            bar(i, y(i), 'FaceColor', '#444');
-            bar(i, y_B(i), 'FaceColor', '#888');
-        else
-            bar(i, y_B(i), 'FaceColor', '#888');
-            bar(i, y(i), 'FaceColor', '#444');
-        end
+        %if y_B(i) < y(i)
+        bar(i - xaltoffset + xoffset, y(i), 'BarWidth', 0.4, 'FaceColor', '#444');
+        bar(i + xaltoffset + xoffset, y_B(i), 'BarWidth', 0.4, 'FaceColor', '#BBB');
+        %else
+        %    bar(i, y_B(i), 'BarWidth', 0.4, 'FaceColor', '#888');
+        %    bar(i, y(i), 'BarWidth', 0.4, 'FaceColor', '#444');
+        %end
     end
     xticks(1:length(x))
     xticklabels(x)
@@ -36,13 +70,14 @@ if plotNum == 1
     xtickangle(60);
     
     %axis square;
+    xlim([1 - 0.6 + xoffset, length(y) + 0.6 + xoffset])
     ylim([0, 1.15])
     set(gca, 'YGrid', 'on', 'YMinorGrid', 'on');
     %set(gca, 'XScale', 'log', 'XTick',x_avg, 'XTickLabel', x_avg, 'YGrid', 'on', 'YMinorGrid', 'on');
     %set(gca, 'XTick',x_avg, 'XTickLabel', x_avg_real, 'YGrid', 'on', 'YMinorGrid', 'on');
     set(gca,'FontSize', fontSize);
     
-    legend('Bytes of code', 'Number of files', 'Location', 'NorthWest', 'NumColumns', 2)
+    legend('Number of files', 'Bytes of code', 'Location', 'NorthWest', 'NumColumns', 2)
     % ax = gca
     % ax.XAxis.FontSize = fontSize;
     % ax.YAxis.FontSize = fontSize;
@@ -65,7 +100,7 @@ elseif plotNum == 2
     tree_construction_ci = read_files_y_ci + make_tree_y_ci
     
     hold on;
-    bar(tree_construction, 'FaceColor', '#026873');
+    bar(x + xoffset, tree_construction, 'FaceColor', '#0593A2');
 %     bar(read_files_y + make_tree_y + generate_proof_y + verify_proof_y, 'FaceColor', '#F00');
 %     bar(read_files_y + make_tree_y + generate_proof_y, 'FaceColor', '#0F0');
 %     bar(read_files_y + make_tree_y, 'FaceColor', '#00F');
@@ -80,10 +115,12 @@ elseif plotNum == 2
 %     
     for i = 1:length(x)
         hold on;
-        plotConfidenceInterval(i, tree_construction(i), tree_construction_ci(i))
+        plotConfidenceInterval(i + xoffset, tree_construction(i), tree_construction_ci(i))
     end
 
-    ylim([45, 250])
+    xlim([1 - 0.6 + xoffset, length(x) + 0.6 + xoffset])
+    ylim([40, 240])
+    yticks([40:20:240])
     
     %set(gca, 'YScale', 'log');
     %bar(y, 'FaceColor', '#444');
@@ -91,7 +128,7 @@ elseif plotNum == 2
     xticklabels(x_str)
     
     xlabel('srsRAN Version')
-    ylabel('Tree Assembly (ms)')
+    ylabel('Time (ms)')
     xtickangle(60);
     
     %axis square;
@@ -116,8 +153,8 @@ elseif plotNum == 3
     read_files_y_ci = data2(:, 3);
     make_tree_y = data2(:, 4);
     make_tree_y_ci = data2(:, 5);
-    generate_proof_y = data2(:, 6);
-    generate_proof_y_ci = data2(:, 7);
+%     generate_proof_y = data2(:, 6);
+%     generate_proof_y_ci = data2(:, 7);
     verify_proof_y = data2(:, 8);
     verify_proof_y_ci = data2(:, 9);
 
@@ -125,18 +162,20 @@ elseif plotNum == 3
     tree_construction_ci = read_files_y_ci + make_tree_y_ci
     
     hold on;
-    bar(generate_proof_y + verify_proof_y, 'FaceColor', '#F2668B');
-    bar(verify_proof_y, 'FaceColor', '#03A688');
+    %bar(generate_proof_y + verify_proof_y, 'FaceColor', '#F2668B');
+    bar(x + xoffset, verify_proof_y, 'FaceColor', '#FF7A48');
+%     for i = 1:length(x)
+%         hold on;
+%         plotConfidenceInterval(i, generate_proof_y(i) + verify_proof_y(i), generate_proof_y_ci(i))
+%     end
     for i = 1:length(x)
         hold on;
-        plotConfidenceInterval(i, generate_proof_y(i) + verify_proof_y(i), generate_proof_y_ci(i))
+        plotConfidenceInterval(i + xoffset, verify_proof_y(i), verify_proof_y_ci(i))
     end
-    for i = 1:length(x)
-        hold on;
-        plotConfidenceInterval(i, verify_proof_y(i), verify_proof_y_ci(i))
-    end
+    xlim([1 - 0.6 + xoffset, length(x) + 0.6 + xoffset])
     %ylim([70, 190])
-    ylim([0, 0.23])
+    ylim([0.03, 0.12])
+    yticks([0.03:0.01:0.12])
 
     xticks(x)
     xticklabels(x_str)
@@ -152,7 +191,7 @@ elseif plotNum == 3
     %set(gca, 'XTick',x_avg, 'XTickLabel', x_avg_real, 'YGrid', 'on', 'YMinorGrid', 'on');
     set(gca,'FontSize', fontSize);
     
-    legend('Proof Generation', 'Proof Verification', 'Location', 'NorthWest', 'NumColumns', 1)
+    %legend('Proof Verification', 'Location', 'NorthWest', 'NumColumns', 1)
     % ax = gca
     % ax.XAxis.FontSize = fontSize;
     % ax.YAxis.FontSize = fontSize;
